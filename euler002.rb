a2a4b874368ff042475fbb0074fe87b92ae3d5c5
wryou <wryou@toydea.com>
2018-09-13 17:54:36 +0900

    problem 002 solved

--- euler002.rb
+++ euler002.rb
@@ -1,3 +1,11 @@
 def euler002
-    0
+    n1 = 1
+    n2 = 2
+    ans = 2
+    begin
+        sum = n1 + n2
+        ans = ans + sum if sum % 2 == 0
+        n1, n2 = n2, sum
+    end until sum >= 4000000
+    return ans
 end
