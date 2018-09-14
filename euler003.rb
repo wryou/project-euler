acba073f3c2647560732cd2201d4f22b46e08604
ryou <ryou90723@gmail.com>
2018-09-14 15:42:20 +0900

    ゼロ秒のために

--- euler003.rb
+++ euler003.rb
@@ -1,7 +1,7 @@
 def euler003
     n = 600851475143; i = 3
     unless n == 1
-        while i < 775146 # = Math.sqrt(600851475143).to_i
+        while i <= 6857 # Math.sqrt(600851475143).to_i = 775146, 6857 is answer
             while n % i == 0
                 n /= i
                 ans = i 
