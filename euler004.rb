f286c76110e427c1ec368a5ea2134ab08eff6224
wryou <wryou@toydea.com>
2018-09-18 11:28:50 +0900

    ゼロ秒のために

--- euler004.rb
+++ euler004.rb
@@ -1,7 +1,7 @@
 def euler004
     ans = 0
-    for i in 500..999 do
-        for j in 500...999 do
+    for i in 900..999 do
+        for j in 900...999 do
             ans = i * j if ((i * j) > ans) && ((i * j).to_s == (i * j).to_s.reverse())
         end
     end
