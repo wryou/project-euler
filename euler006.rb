cb326fd21e2a844f14e9139e28d2e86cecd232bc
wryou <wryou@toydea.com>
2018-09-27 18:22:15 +0900

    ＼(^o^)／

--- euler006.rb
+++ euler006.rb
@@ -1,7 +1,7 @@
 def euler006
     ans = 0
     for i in 1 .. 100 do
-        ans = ans + i ** 2
+        ans += i ** 2
     end
     ((1 + 100) * 100 / 2) ** 2 - ans
 end
--- euler010.rb
+++ euler010.rb
@@ -1,36 +1,10 @@
-# divide solution
-# 
-# def euler010
-#     prime = 0
-#     for num in 2..2000000 do
-#         divide = 2
-#         flag = true
-#         while divide <= Math.sqrt(num)
-#             flag = false if num % divide == 0
-#             break if num % divide == 0
-#             divide += 1
-#         end
-#         prime += num if flag == true
-#     end
-#     prime
-# end
-
-# sieve solutions
 def euler010
-    range = 2000000
-    sieve = []
-    sum = 2
-    prime = 3
-    for idx in 1..range do
-        sieve[idx] = true
-    end
+    range = 2000000; prime = 3; sieve = Array.new(range + 1, true); idx = 3; sum = 2;
     while prime <= Math.sqrt(range)
-        divide = 2
-        flag = true
+        divide = 2; flag = true
         while divide <= Math.sqrt(prime)
             if prime % divide == 0
-                flag = false
-                break 
+                flag = false; break 
             end
             divide += 1
         end
@@ -43,7 +17,6 @@ def euler010
         end
         prime += 2
     end
-    idx = 3
     while idx <= range
         sum += idx if sieve[idx]
         idx += 2
