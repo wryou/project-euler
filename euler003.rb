a0e001be4b7a28d05b50e55b2a87bfc341572d4d
ryou <ryou90723@gmail.com>
2018-10-02 19:51:59 +0900

    modify problem 003

--- euler003.rb
+++ euler003.rb
@@ -1,12 +1,9 @@
 def euler003
-    n = 600851475143; i = 3
-    unless n == 1
-        while i <= 6857 # Math.sqrt(600851475143).to_i = 775146, 6857 is answer
-            while n % i == 0
-                n /= i
-                ans = i 
-            end
-            i += 2
+    n = 600851475143; ans = 1
+    3.step(Math.sqrt(n).to_i, 2).each do |i|
+        while n % i == 0 && n != 1
+            n /= i
+            ans = i 
         end
     end
     ans
