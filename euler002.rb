d8517321fb172637d351c8efeb79d2edaf56df09
ryou <ryou90723@gmail.com>
2018-10-02 19:51:54 +0900

    modify problem 002

--- euler002.rb
+++ euler002.rb
@@ -1,7 +1,7 @@
 def euler002
     n1 = 1; n2 = 2; ans = n2
     begin
-        ans += (n1 + n2) if (n1 + n2) % 2 == 0
+        ans += (n1 + n2) if (n1 + n2).even?
         n1, n2 = n2, (n1 + n2)
     end until (n1 + n2) >= 4000000
     ans
