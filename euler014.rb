cd13f9491fe02821a99ded3c5100e559956c8f4a
ryou <ryou90723@gmail.com>
2018-10-02 14:09:23 +0900

    problem 014 solved

--- euler014.rb
+++ euler014.rb
@@ -1,19 +1,14 @@
 def euler014
     maxIdx = 0; maxLen = 0
-    500001.step(1000000, 2).each do |current|
-        length = 0
-        num = current
-        until num == 1 do
-            if num.even?
-                num /= 2
-            else
-                num = num * 3 + 1
-            end
+    500001.step(1000000, 2).each do |crtIdx|
+        idx = crtIdx; length = 0
+        until idx == 1 do
+            idx = (idx.even?) ? idx / 2 : idx * 3 + 1
             length += 1
         end
         if length > maxLen
             maxLen = length
-            maxIdx = current
+            maxIdx = crtIdx
         end
     end
     maxIdx
