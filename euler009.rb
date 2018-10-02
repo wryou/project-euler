c64c7b957a272aec6f5dc8c924b5e4b0d12170b1
ryou <ryou90723@gmail.com>
2018-10-02 19:52:27 +0900

    modify problem 009

--- euler009.rb
+++ euler009.rb
@@ -1,9 +1,7 @@
 def euler009
-    for a in 1..500 do
-        for b in 1..500 do
-            if Math.sqrt(a**2 + b**2) % 1 == 0
-                return (a * b * (1000 - a - b)) if (a + b + Math.sqrt(a**2 + b**2)) == 1000
-            end
+    (1..500).each do |i|
+        (1..500).each do |j|
+            return i * j * (1000 - i - j) if i + j + Math.sqrt(i ** 2 + j ** 2) == 1000
         end
     end
 end
