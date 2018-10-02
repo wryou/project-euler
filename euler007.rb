e80859bb65318cb0d9dc62966c4859fa9eee85ec
ryou <ryou90723@gmail.com>
2018-10-02 19:52:14 +0900

    modify problem 007

--- euler007.rb
+++ euler007.rb
@@ -1,14 +1,14 @@
 def euler007
-    idx = 1; prime = 2; num = 3
+    idx = 1; prime = 2; num = 1
     until idx == 10001
-        divide = 2; flag = true
+        num += 2; divide = 2; flag = true
         while divide <= Math.sqrt(num)
             flag = false if num % divide == 0
             divide += 1
         end
-        idx += 1 if flag
-        prime = num if flag
-        num += 2
+        if flag 
+            idx += 1; prime = num
+        end
     end
     prime
 end
