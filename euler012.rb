38a7c2ed465e357402b69334cd8dedd0aee80c23
ryou <ryou90723@gmail.com>
2018-10-02 19:52:37 +0900

    modify problem 012

--- euler012.rb
+++ euler012.rb
@@ -3,7 +3,7 @@ def euler012
     tri = 0; iter = 1; goal = 500; mul = 1
     until mul >= goal
         tri += iter; iter += 1; mul = 1
-        for power in 0..Prime.prime_division(tri).size - 1 do
+        (0..Prime.prime_division(tri).size - 1).each do |power|
             mul *= (Prime.prime_division(tri)[power][1].to_i + 1)
         end
     end
